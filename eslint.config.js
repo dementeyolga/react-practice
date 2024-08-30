2a0b51845df8a41b89c3dccf4795ccc7b8b8cadf
dementeyolga <dementeyolya@gmail.com>
2024-08-30 13:00:02 +0300

    core: setup project dependencies (eslint, prettier, husky)

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import reactCompiler from 'eslint-plugin-react-compiler';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      eslintConfigPrettier,
+    ],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'react-compiler': reactCompiler,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      '@typescript-eslint/no-explicit-any': 'error',
+      'react-compiler/react-compiler': 'error',
+    },
+  },
+);
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <p>hi class components</p>
+    </>
+  );
+}
+
+export default App;
